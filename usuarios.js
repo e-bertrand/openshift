04dc9d31acdf77d4f978724ad67cafbb08e2e942
Enrique <e-bertrand@users.noreply.github.com>
2019-09-05 23:32:36 +0200

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const usuarios = [
+  {nombre: 'José', posicion: 'programador'},
+  {nombre: 'Carmen', posicion: 'coordinadora'},
+  {nombre: 'Juan', posicion: 'analista'},
+  {nombre: 'Marta', posicion: 'externa'}
+];
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la aplicacion de usuarios');
+});
+
+app.get('/usuarios', (req, res, next) => {
+  res.send(usuarios);
+});
+
+app.get('/usuarios/:id', (req, res, next) => {
+  const id = req.params.id - 1;
+  console.log('Buscando usuario:', id);
+  if (usuarios[id] == null) {
+    res.send('Usuario no existe');
+  } else {
+    res.send(usuarios[id]);
+  }
+});
+
+const server = app.listen(8888, function() {
+  console.log('Servidor iniciado..');
+});
